bb90d720fbd8dd8780decbde13f00d7bf72c0588
Algerdi <kzlv.vldslv@gmail.com>
2022-04-28 14:47:07 +0300

    Chat added

---
+++ back/mychat/mychat/asgi.py
@@ -0,0 +1,33 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
+
+
+
+
+
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+
+# application = get_asgi_application()
